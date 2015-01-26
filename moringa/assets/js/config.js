147aef5f1a55c9bac2d11c4a96e523d7ca3ae20d
Leon Mergen <leon@solatis.com>
2015-01-26 07:43:13 +0000

    Initial check in

---
+++ moringa/assets/js/config.js
@@ -0,0 +1,31 @@
+/*
+	Arcana 2.1 by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+window._skel_config = {
+	preset: 'standard',
+	prefix: 'assets/css/style',
+	resetCSS: true
+};
+
+window._skel_panels_config = {
+	preset: 'standard'
+};
+
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
